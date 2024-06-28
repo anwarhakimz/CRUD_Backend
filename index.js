6aca5a344859788584ea9283d19304ad257f22b2
anwarhakim31 <anwarhakim31@gmail.com>
2024-06-28 11:16:51 +0700

    backebd

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ dotenv.config();
 mongoose.set("strictQuery", true);
 // Connect to MongoDB Atlas using the URI from environment variables
 mongoose
-  .connect(process.env.uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://hakim:310501@cluster0.1hmc4qg.mongodb.net/CRUD_MERN?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Database Connected..."))
   .catch((error) => console.log("Connection error:", error));
 
 // Set the port from environment variables
-const port = process.env.PORT_URL;
+const port = 8000;
 
 web.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
